d9ec3de84b8f52de3103f86362b6bfdf5d7701d0
ahmad <ahmad_mahmoud@gmail.com>
2023-02-22 16:50:09 +0200

    create project, setup dashboard and create invoices modules

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\InvoiceController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+
+
+Auth::routes();
+// Auth::routes(['register' => false]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+// Invoices Routes
+Route::resource('invoices', InvoiceController::class);
+
+
+
+
+Route::get('/{page}', [AdminController::class, 'index']);
+
